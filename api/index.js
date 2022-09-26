5c94b180fb20c89dbf4ff3be988a2f8e77447439
MBertold <marco_bertoldi@hotmail.it>
2022-09-26 18:15:08 +0200

    frontend

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const userRoute = require("./routes/user")
@@ -13,6 +13,6 @@ mongoose.connect(
     ).then(()=>console.log("connessione riuscita")).catch((err)=> {console.log(err)})
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 app.use("/api/user", userRoute);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
